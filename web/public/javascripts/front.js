fa1e0d7d801dcc9ca7dd31d92ed3ff37141a85af
Francisco Romero <francisco.romero@cesurformacion.com>
2024-10-02 14:28:08 +0200

    Footer + offcanvas + section

--- web/public/javascripts/front.js
+++ web/public/javascripts/front.js
@@ -1,1 +1,11 @@
 console.log("Hola mundo...");
+
+document.querySelector("input#lenguaje").value = navigator.userAgent;
+
+document.querySelector("a#open-offcanvas").addEventListener("click",()=>{
+    document.querySelector("aside#offcanvas").classList.add("show");
+});
+
+document.querySelector("#offcanvas button.close").addEventListener("click",()=>{
+    document.querySelector("aside#offcanvas").classList.remove("show");
+});
