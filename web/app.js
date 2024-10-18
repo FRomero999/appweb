dd98e7126756eeb468f066a8f7f9f7e34ecaf382
Francisco Romero <francisco.romero@cesurformacion.com>
2024-10-18 10:47:46 +0200

    otro error forzado

--- web/app.js
+++ web/app.js
@@ -16,9 +16,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'publica')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', indexRouter
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
